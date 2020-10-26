99b681c1e21485c97879a3bf98bde599d108c54e
Zhang Zhanpeng <zhangregister@outlook.com>
2020-10-27 00:31:11 +0800

    lcci#01.01

---
+++ lcci/01.01-is-unique-lcci/is-unique-lcci.go
@@ -0,0 +1,12 @@
+package is_unique_lcci
+
+func isUnique(astr string) bool {
+	elemMap := make(map[string]bool)
+	for i := 0; i < len(astr); i++ {
+		if _, ok := elemMap[astr[i:i+1]]; ok {
+			return false
+		}
+		elemMap[astr[i:i+1]] = true
+	}
+	return true
+}

0b5ffbbfd0a5bf9d65970248162c22dd3e80b53f
yunxiaozhao <zhao.yunxiao@outlook.com>
2021-03-22 15:43:56 +0800

    为分治法添加随机选择

--- zhao-c++/215-kth-largest-element-in-an-array/partition.cpp
+++ zhao-c++/215-kth-largest-element-in-an-array/partition.cpp
@@ -4,28 +4,36 @@ public:
         int left = 0;
         int right = nums.size() - 1;
         while(true) {
-            int a = partition(nums, left, right);
-            if(a+1 == k) {
-                return nums[a];
-            } else if (a < k) {
-                left = a+1;
+            int pivot = partition(nums, left, right);
+            if(pivot+1 == k) {
+                return nums[pivot];
+            } else if (pivot < k) {
+                left = pivot+1;
             } else {
-                right = a-1;
+                right = pivot-1;
             }
 
         }
     }
 
     int inline partition(vector<int>& nums, int left, int right) {
+        randomChoose(nums, left, right);
         int j = left;
         for(int i = left+1; i<=right; i++) {
-            if(nums[i] >= nums[left] && ++j!=i) {
-            	swap(nums, j, i);
+            if(nums[i] >= nums[left] && ++j!=i){
+                swap(nums, j, i);
             }
         }
         swap(nums, left, j);
         return j;
-   } 
+    } 
+
+    void inline randomChoose(vector<int>& nums, int left, int right) {
+        srand((unsigned)time(NULL));
+        int index = (rand() % (right-left+1))+ left;
+        swap(nums, left, index);
+    }
+
     void inline swap(vector<int>& nums, int left, int right) {
         int temp = nums[left];
         nums[left] = nums[right];

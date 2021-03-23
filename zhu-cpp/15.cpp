6d2e81546081d17d2c911816cdcc5e32a8e549e5
oOlivero <liuzunxiong@qq.com>
2021-03-24 02:11:03 +0800

    15

--- zhu-cpp/15.cpp
+++ zhu-cpp/15.cpp
@@ -11,48 +11,92 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>>r;
-        vector<int>tnums;
         if(nums.size()<3)
             return r;
         map<int,int> m;
-        map<vector<int>,int> mv;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++) {
-            if(m[nums[i]]<3)
+        for(int i=0;i<nums.size()&&nums[i]<=0;i++) {
+            while(i>0&&i<nums.size()&&nums[i]==nums[i-1])
+                i++;
+            int j=i+1,k=nums.size()-1;
+            while(j<k)
             {
-                tnums.push_back(nums[i]);
-                m[nums[i]]++;
+                vector<int>t;
+                if(nums[j]+nums[k]+nums[i]==0)
+                {
+                    cout<<"i:"<<i<<" j:"<<j<<" k:"<<k<<endl;
+                    t.push_back(nums[i]);
+                    t.push_back(nums[j]);
+                    t.push_back(nums[k]);
+                    r.push_back(t);
+                    while(k>j&&nums[k]==nums[k-1])
+                        k--;
+                    k--,j++;
+                }
+                else if(nums[j]+nums[k]+nums[i]>0)
+                {
+                    k--;
+                }
+                else j++;
             }
         }
-        for(int i=0;i<tnums.size()-1&&tnums[i]<=0;i++)
-            for(int j=i+1;j<tnums.size();j++)
-            {
-                vector<int> t;
-                m[tnums[i]]--;
-                m[tnums[j]]--;
-                if(tnums[j]<=-tnums[i]-tnums[j])
-                    if(m[-tnums[i]-tnums[j]]>0)
-                    {
-                        t.push_back(tnums[i]);
-                        t.push_back(tnums[j]);
-                        t.push_back(-tnums[i]-tnums[j]);
-                        if(!mv[t])
-                        {
-                            r.push_back(t);
-                            mv[t]=1;
-                        }
-                    }
-                m[tnums[i]]++;
-                m[tnums[j]]++;
-            }
         return r;
     }
 };
+//    vector<vector<int>> threeSum(vector<int>& nums) {
+//        vector<vector<int>>r;
+//        vector<int>tnums;
+//        if(nums.size()<3)
+//            return r;
+//        map<int,int> m;
+//        map<vector<int>,int> mv;
+//        sort(nums.begin(),nums.end());
+//        for(int i=0;i<nums.size();i++) {
+//            if(m[nums[i]]<3)
+//            {
+//                tnums.push_back(nums[i]);
+//                m[nums[i]]++;
+//            }
+//        }
+//        for(int i=0;i<tnums.size()-1&&tnums[i]<=0;i++)
+//            for(int j=i+1;j<tnums.size();j++)
+//            {
+//                vector<int> t;
+//                m[tnums[i]]--;
+//                m[tnums[j]]--;
+//                if(tnums[j]<=-tnums[i]-tnums[j])
+//                    if(m[-tnums[i]-tnums[j]]>0)
+//                    {
+//                        t.push_back(tnums[i]);
+//                        t.push_back(tnums[j]);
+//                        t.push_back(-tnums[i]-tnums[j]);
+//                        if(!mv[t])
+//                        {
+//                            r.push_back(t);
+//                            mv[t]=1;
+//                        }
+//                    }
+//                m[tnums[i]]++;
+//                m[tnums[j]]++;
+//            }
+//        return r;
+//    }
 
 int main()
 {
     Solution s;
-    cout<<s.threeSum(a)<<endl;
+    int a[] = {-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6};
+    vector<int> aa;
+    vector<vector<int>>r;
+    for(int i=0;i<15;i++)
+        aa.push_back(a[i]);
+    r = s.threeSum(aa);
+    for(int i=0;i<r.size();i++)
+    {
+        for(int j=0;j<r[i].size();j++)
+            cout<<r[i][j]<<" ";
+        cout<<endl;
+    }
     return 1;
 }
 //a+b=-c;
